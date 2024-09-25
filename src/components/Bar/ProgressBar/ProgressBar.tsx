453666386713b0c01a1053c92fbe7e08738117f7
Yulenka5 <yulia1995m@mail.ru>
2024-09-25 17:15:25 +0500

    hw3

---
+++ src/components/Bar/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,22 @@
+import styles from "./ProgressBar.module.css"
+
+type ProgressBarProps = {
+    max: number,
+    value: number,
+    step: number,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>)=> void
+}
+function ProgressBar  ({ max, value, step, onChange }: ProgressBarProps)  {
+        return (
+            <input
+                className={styles.styledProgressInput}
+                type="range"
+                min={0}
+                max={max}
+                value={value}
+                step={step}
+                onChange={onChange}
+            />
+        )}
+
+export default ProgressBar
